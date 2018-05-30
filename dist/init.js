1f2a619c8ef5167a20f2d153797dc262b0614151
Tomasz Ćwik <t.cwika@clickonometrics.pl>
2018-05-30 21:20:28 +0200

    gicio

--- dist/init.js
+++ dist/init.js
@@ -1,44 +1,101 @@
 var Init = /** @class */ (function () {
+    /**
+     * Start
+     *
+     * @param socket Socket.io
+     */
     function Init(socket) {
-        var self = this;
+        var _this = this;
+        this.token = 'pk.eyJ1IjoidG9jdzg2IiwiYSI6ImNqaHM0YTh2bzA3bDUzN254Mndyb2c4dm0ifQ.3eIb7F5PV-E6pBugRhs4cQ';
+        /**
+         * Set user safe data
+         * @return void
+         */
+        this.setUserDate = function (position) {
+            _this.lat = position.coords.latitude;
+            _this.lng = position.coords.longitude;
+            _this.user_id = Math.random().toString(36).substring(7);
+        };
+        /**
+         * Get user data in json format
+         * @return string
+         */
+        this.getJsonFromUser = function () {
+            return JSON.stringify({
+                lat: _this.lat,
+                lng: _this.lng,
+                user_id: _this.user_id
+            });
+        };
+        /**
+         * Send user data to socket
+         * @return void
+         */
+        this.sendUserData = function () {
+            _this.socket.emit('new_user', _this.getJsonFromUser());
+        };
+        /**
+         * Trigger all socket events
+         * @return void
+         */
+        this.triggerSocketEvents = function () {
+            _this.socket.on('load_users', function (usersData) {
+                var data = JSON.parse(usersData);
+                for (var i = 0; i < data.length; i++) {
+                    L.marker([data[i].lat, data[i].lng]).addTo(this.map);
+                }
+            });
+            _this.socket.on('update_markers', function (usersData) {
+                var data = JSON.parse(usersData);
+                L.marker([data.lat, data.lng]).addTo(this.map);
+            });
+        };
+        /**
+         * Init map
+         */
+        this.initMap = function () {
+            _this.map = L.map('map').setView([_this.lat, _this.lng], 18);
+            L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + _this.token, {
+                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                maxZoom: 18,
+                id: 'mapbox.streets',
+                accessToken: _this.token
+            }).addTo(_this.map);
+        };
+        /**
+         * Set user marker
+         * @return void
+         */
+        this.setUserMarker = function () {
+            _this.marker = L.marker([_this.lat, _this.lng]).addTo(_this.map);
+        };
+        /**
+         * Run after get geo action
+         */
+        this.run = function (position) {
+            _this.setUserDate(position);
+            _this.sendUserData();
+            _this.windowEvents();
+            _this.initMap();
+            _this.setUserMarker();
+        };
         this.socket = socket;
         if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function (position) {
-                self.lat = position.coords.latitude;
-                self.lng = position.coords.longitude;
-                self.user_id = Math.random().toString(36).substring(7);
-                socket.emit('new_user', JSON.stringify({
-                    lat: self.lat,
-                    lng: self.lng,
-                    user_id: self.user_id
-                }));
-                window.onunload = function () {
-                    socket.emit('remove_user', self.user_id);
-                };
-                var token = 'pk.eyJ1IjoidG9jdzg2IiwiYSI6ImNqaHM0YTh2bzA3bDUzN254Mndyb2c4dm0ifQ.3eIb7F5PV-E6pBugRhs4cQ';
-                var map = L.map('map').setView([self.lat, self.lng], 18);
-                L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + token, {
-                    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-                    maxZoom: 18,
-                    id: 'mapbox.streets',
-                    accessToken: token
-                }).addTo(map);
-                var marker = L.marker([self.lat, self.lng]).addTo(map);
-                socket.on('load_users', function (usersData) {
-                    var data = JSON.parse(usersData);
-                    for (var i = 0; i < data.length; i++) {
-                        L.marker([data[i].lat, data[i].lng]).addTo(map);
-                    }
-                });
-                socket.on('update_markers', function (usersData) {
-                    var data = JSON.parse(usersData);
-                    L.marker([data.lat, data.lng]).addTo(map);
-                });
-            });
+            navigator.geolocation.getCurrentPosition(this.run);
         }
         else {
             alert("Geolocation is not supported by this browser.");
         }
     }
+    /**
+     * Trigger window events
+     * @return void
+     */
+    Init.prototype.windowEvents = function () {
+        var self = this;
+        window.onunload = function () {
+            self.socket.emit('remove_user', self.user_id);
+        };
+    };
     return Init;
 }());
--- src/init.ts
+++ src/init.ts
@@ -1,63 +1,128 @@
 class Init {
 
+    private token: string = 'pk.eyJ1IjoidG9jdzg2IiwiYSI6ImNqaHM0YTh2bzA3bDUzN254Mndyb2c4dm0ifQ.3eIb7F5PV-E6pBugRhs4cQ';
     private lat: number;
     private lng: number;
     private user_id: string;
-    public socket: any;
+    private map: any;
+    private socket: any;
+    private marker:any;
 
+    /**
+     * Start
+     * 
+     * @param socket Socket.io
+     */
     constructor(socket: any) {
-        var self = this;
+
         this.socket = socket;
 
         if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function (position) {
-
-                self.lat = position.coords.latitude;
-                self.lng = position.coords.longitude;
-                self.user_id = Math.random().toString(36).substring(7);
-
-                socket.emit('new_user', JSON.stringify({
-                    lat: self.lat,
-                    lng: self.lng,
-                    user_id: self.user_id
-                }));
-
-                window.onunload = function () {
-                    socket.emit('remove_user', self.user_id);
-                }
-
-
-                var token = 'pk.eyJ1IjoidG9jdzg2IiwiYSI6ImNqaHM0YTh2bzA3bDUzN254Mndyb2c4dm0ifQ.3eIb7F5PV-E6pBugRhs4cQ';
-                var map = L.map('map').setView([self.lat, self.lng], 18);
-
-                L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + token, {
-                    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-                    maxZoom: 18,
-                    id: 'mapbox.streets',
-                    accessToken: token
-                }).addTo(map);
-
-                var marker = L.marker([self.lat, self.lng]).addTo(map);
-
-                socket.on('load_users', function (usersData: any) {
-                    var data = JSON.parse(usersData);
-                    for (var i = 0; i < data.length; i++) {
-                        L.marker([data[i].lat, data[i].lng]).addTo(map);
-                    }
-                });
-                socket.on('update_markers', function (usersData: any) {
-                    var data = JSON.parse(usersData);
-                    L.marker([data.lat, data.lng]).addTo(map);
-
-                });
-
-            });
+            navigator.geolocation.getCurrentPosition(this.run);
         } else {
             alert("Geolocation is not supported by this browser.");
         }
+    }
 
+    /**
+     * Trigger window events
+     * @return void
+     */
+    private windowEvents(): void {
+        var self = this;
+        window.onunload = function () {
+            self.socket.emit('remove_user', self.user_id);
+        }
+    }
+
+    /**
+     * Set user safe data
+     * @return void
+     */
+    private setUserDate = (position: any): void => {
+        this.lat = position.coords.latitude;
+        this.lng = position.coords.longitude;
+        this.user_id = Math.random().toString(36).substring(7);
+    }
+
+    /**
+     * Get user data in json format
+     * @return string
+     */
+    private getJsonFromUser = (): string => {
+        return JSON.stringify({
+            lat: this.lat,
+            lng: this.lng,
+            user_id: this.user_id
+        });
+    }
+
+    /**
+     * Send user data to socket
+     * @return void
+     */
+    private sendUserData = (): void => {
+        this.socket.emit('new_user', this.getJsonFromUser());
+    }
+
+    /**
+     * Trigger all socket events
+     * @return void
+     */
+    private triggerSocketEvents = (): void => {
+
+        this.socket.on('load_users', function (usersData: any) {
+            var data = JSON.parse(usersData);
+            for (var i = 0; i < data.length; i++) {
+                L.marker([data[i].lat, data[i].lng]).addTo(this.map);
+            }
+        });
+
+        this.socket.on('update_markers', function (usersData: any) {
+            var data = JSON.parse(usersData);
+            L.marker([data.lat, data.lng]).addTo(this.map);
+        });
 
     }
 
+    /**
+     * Init map
+     */
+    private initMap = ():void => {
 
+        this.map = L.map('map').setView([this.lat, this.lng], 18);
+
+        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + this.token, {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox.streets',
+            accessToken: this.token
+        }).addTo(this.map);
+
+    }
+
+    /**
+     * Set user marker
+     * @return void
+     */
+    private setUserMarker = ():void => {
+        this.marker = L.marker([this.lat, this.lng]).addTo(this.map);
+    }
+
+    /**
+     * Run after get geo action
+     */
+    public run = (position: any) => {
+
+        this.setUserDate(position)
+
+        this.sendUserData();
+
+        this.windowEvents();
+ 
+        this.initMap();
+       
+        this.setUserMarker();
+
+    }
 }
