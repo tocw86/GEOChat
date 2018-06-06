a2448f2b48c40666690686f84b9517373d48186d
Tomasz Ćwik <t.cwika@clickonometrics.pl>
2018-06-06 23:05:22 +0200

    handshake

--- src/init.ts
+++ src/init.ts
@@ -8,34 +8,36 @@ class Init {
     private socket: any;
     private marker: any;
     private usersMarkers: Array<any> = [];
-    private icons:any = {
-       red: L.icon({
+    private icons: any = {
+        red: L.icon({
             iconUrl: 'leaflet/images/marker-icon-red.png',
             shadowUrl: 'leaflet/images/marker-shadow.png',
         }),
-       green: L.icon({
+        green: L.icon({
             iconUrl: 'leaflet/images/marker-icon-green.png',
             shadowUrl: 'leaflet/images/marker-shadow.png',
         }),
-       blue: L.icon({
+        blue: L.icon({
             iconUrl: 'leaflet/images/marker-icon.png',
             shadowUrl: 'leaflet/images/marker-shadow.png',
         }),
     };
     private defaultPosition = {
         coords: {
-            latitude: 51.1739726374,
-            longitude: -1.82237671048
+            // latitude: 51.1739726374,
+            // longitude: -1.82237671048
+            latitude: 37.629562,
+            longitude: -116.849556
         }
     };
-    private markerType:string;
+    private markerType: string;
 
     /**
      * Start
      * 
      * @param socket Socket.io
      */
-    constructor(socket: any, markerType:string) {
+    constructor(socket: any, markerType: string) {
 
         this.socket = socket;
         this.markerType = markerType;
@@ -46,11 +48,11 @@ class Init {
             alert("Geolocation is not supported by this browser.");
         }
     }
-    
+
     public error = () => {
         this.run(this.defaultPosition);
     }
-    
+
     /**
     * Run after get geo action
     */
@@ -78,8 +80,24 @@ class Init {
     private windowEvents(): void {
         var self = this;
         window.addEventListener('beforeunload', function (e) {
-            self.socket.emit('remove_user', self.user_id);
+            //self.socket.emit('remove_user', self.user_id);
         });
+    }
+
+    /**
+     * get random string
+     * 
+     * @param len 
+     * @param charSet 
+     */
+    private randomString(len: number, charSet?: string): string {
+        charSet = charSet || 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+        var randomString = '';
+        for (var i = 0; i < len; i++) {
+            var randomPoz = Math.floor(Math.random() * charSet.length);
+            randomString += charSet.substring(randomPoz, randomPoz + 1);
+        }
+        return randomString;
     }
 
     /**
@@ -89,7 +107,7 @@ class Init {
     private setUserDate = (position: any): void => {
         this.lat = position.coords.latitude;
         this.lng = position.coords.longitude;
-        this.user_id = Math.random().toString(36).substring(7);
+        this.user_id = this.randomString(32);
     }
 
     /**
@@ -114,6 +132,28 @@ class Init {
     }
 
     /**
+     * Factory for load users
+     * @param lat 
+     * @param lng 
+     * @param user_id 
+     * @returns marker
+     */
+    private markerFactory = (lat:number, lng:number, user_id:string, markerType:string):any => {
+        var icon = this.icons[markerType];
+        var marker = L.marker([lat, lng], { icon: icon }).addTo(this.map).on('click',function(event){
+            console.log(this.getLatLng());
+        });
+        marker.bindPopup('<p>' + user_id + '<br/><button id="'+user_id+'">Handshake</button></p>').openPopup();
+ 
+             document.getElementById(user_id).addEventListener('click', function(e){
+                 console.log(user_id);
+             });
+ 
+
+        return marker;
+    }
+
+    /**
      * Trigger all socket events
      * @return void
      */
@@ -128,8 +168,7 @@ class Init {
 
             for (var i = 0; i < data.length; i++) {
                 if (data[i].user_id != self.user_id) {
-                    var icon = self.icons[data[i].markerType];
-                    var marker = L.marker([data[i].lat, data[i].lng],{icon:icon}).addTo(self.map);
+                    var marker = self.markerFactory(data[i].lat,data[i].lng,data[i].user_id,data[i].markerType);
                     self.usersMarkers.push(
                         {
                             user_id: data[i].user_id,
@@ -148,8 +187,9 @@ class Init {
             var data = JSON.parse(usersData);
 
             if (data.user_id != self.user_id) {
-                var icon = self.icons[data.markerType];
-                var marker = L.marker([data.lat, data.lng],{icon:icon}).addTo(self.map);
+               
+                var marker = self.markerFactory(data.lat,data.lng,data.user_id,data.markerType);
+
                 self.usersMarkers.push(
                     {
                         user_id: data.user_id,
@@ -196,12 +236,12 @@ class Init {
      */
     private initMap = (): void => {
 
-        this.map = L.map('map').setView([this.lat, this.lng], 12);
+        this.map = L.map('map').setView([this.lat, this.lng], 14);
 
         L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + this.token, {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
             maxZoom: 18,
-            id: 'mapbox.streets',
+            id: 'mapbox.streets-satellite',
             accessToken: this.token
         }).addTo(this.map);
 
@@ -224,6 +264,7 @@ class Init {
     private mapEvents = (): void => {
         var self = this;
         this.map.on('click', function (event: any) {
+
             if (typeof self.marker != 'undefined') {
                 self.lat = event.latlng.lat;
                 self.lng = event.latlng.lng;
@@ -233,6 +274,7 @@ class Init {
 
             }
         });
+
     }
 
     /**
--- src/window.js
+++ src/window.js
@@ -1,11 +1,12 @@
 ;
 (function () {
-
+    var $io = io;
+    io = null;
     document.getElementById('marker-holder').addEventListener('click', function (e) {
         if (e.target.tagName === 'IMG') {
             var attr = e.target.getAttribute('data-marker');
             if (attr != null && attr != '' && ['red','green','blue'].indexOf(attr) > -1) {
-                var socket = io.connect('http://localhost:3000');
+                var socket = $io.connect('http://localhost:3000');
                 var user = new Init(socket, attr);
                 document.getElementById('marker-holder').innerHTML = '';
             }
