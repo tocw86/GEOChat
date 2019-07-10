c0b73b9628bf088f1eecc09142b86b657f6b4f25
tomek ćwik <tomek@redicon.pl>
2019-07-10 13:07:49 +0200

    port

--- app.js
+++ app.js
@@ -160,6 +160,6 @@ io.sockets.on('connection', function (socket) {
 
 
 
-http.listen(3001, function () {
-    console.log('listening on *:3001');
+http.listen(3000, function () {
+    console.log('listening on *:3000');
 });
