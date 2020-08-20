f70bef4de9acd2d5c243fa42fee36b98ad8eabf7
tomek ćwik <tomek@redicon.pl>
2020-08-20 13:55:13 +0200

    fix map

--- src/app/geo/map.ts
+++ src/app/geo/map.ts
@@ -20,16 +20,16 @@ namespace Map {
         }
 
         public initMap(): void {
-            this.map = L.map('map').setView([51.505, -0.09], 13);
+            this.map = L.map('map').setView([this.defaultPosition.coords.latitude, this.defaultPosition.coords.longitude], 14);
 
-            // L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-            //     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            //     maxZoom: 18,
-            //     id: 'mapbox/streets-v11',
-            //     tileSize: 512,
-            //     zoomOffset: -1,
-            //     accessToken: 'pk.eyJ1IjoidG9jdzg2IiwiYSI6ImNrYXh2MzB3dTA5d2IycXFxdXpqdDBzencifQ.QHeTo5TdhHFOBE4uY0KpaQ'
-            // }).addTo(this.map);
+            L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=' + this.token, {
+                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                maxZoom: 16,
+                id: 'mapbox/streets-v11',
+                tileSize: 512,
+                zoomOffset: -1,
+                accessToken: this.token
+            }).addTo(this.map);
 
             // L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}' + this.token, {
             //     attribution: '',
